caea80a08eae2fdeaebf312cab61adb6a7a5ba3e
alexo <alexola.22@gmail.com>
2022-09-08 11:40:44 +0100

    modifications to style components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import ListOfGifs from './components/ListOfGifs';
+import appLogo from './app-logo.png'
 
 
 import { Link, Route } from "wouter";
@@ -9,10 +10,11 @@ export default function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <h1>App</h1>
+        <img className="logo" src={appLogo} alt="logo app"/>
         <Link to='/gif/panda'>Gifs de pandas</Link>
         <Link to='/gif/funny cat'>Gifs de gatos</Link>
         <Link to='/gif/southpark'>Gifs de southpark</Link>
+        <Link to='/gif/matrix'>Gifs de matrix</Link>
         <Route 
           component={ListOfGifs}
           path="/gif/:keyword"  />
